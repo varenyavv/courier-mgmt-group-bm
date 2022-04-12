bb0c922ebdde0bbfd47494ba007b228472905af8
Varenya Varshney <varenya.ind@gmail.com>
2022-04-12 10:58:21 +0530

    Update Status.java

--- src/main/java/bits/mt/ss/dda/groupbm/couriermgmt/enums/Status.java
+++ src/main/java/bits/mt/ss/dda/groupbm/couriermgmt/enums/Status.java
@@ -3,7 +3,7 @@ package bits.mt.ss.dda.groupbm.couriermgmt.enums;
 public enum Status {
   BOOKED,
   IN_TRANSIT,
-  RECEIVED_AT_DESTINATION_BRANCH,
+  RECEIVED_AT_DEST_BRANCH,
   OUT_FOR_DELIVERY,
   DELIVERED,
   UNDELIVERED;
