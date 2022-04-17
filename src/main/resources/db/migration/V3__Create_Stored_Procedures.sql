cd5151b5c2bc9a381dd77334270f798816725165
Vaishnavi Sribalaraj <vaishnavibalaraj28@gmail.com>
2022-04-17 11:48:50 +0530

    Resolved comments

--- src/main/resources/db/migration/V3__Create_Stored_Procedures.sql
+++ src/main/resources/db/migration/V3__Create_Stored_Procedures.sql
@@ -55,3 +55,67 @@ when others then
         p_error := sqlerrm;
 end
 $$
+
+-- Agent_upsert Procedure
+
+create or replace
+procedure upsert_agent
+    (
+    p_is_insert in BOOLEAN,
+    p_contact_num in agent.contact_num%type,
+    p_name in agent.name%type,
+    p_branch_code in agent.branch_code%type,    
+    p_add_line in agent.add_line%type,
+    p_pincode in agent.pincode%type,
+    p_city in agent.city%type,
+    p_state in agent.state%type,
+    p_error inout VARCHAR
+    )
+language plpgsql
+as
+$$
+declare
+-- No variable declarations at this time.
+
+begin
+   if p_is_insert
+   then
+	insert
+	into
+	agent
+    (
+	contact_num,
+	name,
+      branch_code,	
+	add_line,
+	pincode,
+	city,
+	state
+    )
+values (    
+	 p_contact_num,
+     p_name,	 
+     p_branch_code,
+     p_add_line,
+     p_pincode,
+     p_city,
+     upper(p_state));
+else
+    update
+	agent
+set
+      branch_code = p_branch_code,
+	name = p_name,
+	add_line = p_add_line,
+	pincode = p_pincode,
+	city = p_city,
+	state = upper(p_state)
+where
+	contact_num = p_contact_num;
+end if;
+
+exception
+when others then
+        p_error := sqlerrm;
+end
+$$
